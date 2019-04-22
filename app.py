a38e65198b6aead259f394cacad873e0ed2f55aa
ahmedelmahy <elmahy2005@gmail.com>
2019-04-22 22:07:34 +0200

    camera from html

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request, redirect
+import cv2
+import torch
+from torchvision import transforms
+from torch.autograd import Variable
+from PIL import Image
+import time
+from io import StringIO
+import re
+from face_recognize import face_cascade, predict_gender, predict_glass, predict_Chubby
+import base64
+
 
 app = Flask(__name__)
 
@@ -6,12 +17,57 @@ app = Flask(__name__)
 def index(name=None):
     return render_template('index.html',name=name)
 
-@app.route('/exec')
-def parse(name=None):
-    import face_recognize
-    print("done")
-    return render_template('index.html',name=name)
+#background process happening without any refreshing
 
+@app.route('/snap_a_signal', methods=["POST", "GET"])
+def process_signal():
+    pixels = request.get_json()['data']
+    result=im2info(pixels)
+    if result:
+        return jsonify(gender=result[0],
+                       glass=result[1],
+                       chubby=result[2]
+                       )
+    else:
+        return jsonify(result = "0");
+
+def im2info(pixels):
+    dataUrlPattern = re.compile('data:image/(png|jpeg);base64,(.*)$')
+    image_data = dataUrlPattern.match(pixels).group(2)
+    image_data = image_data.encode()
+    image_data = base64.b64decode(image_data)
+ 
+    with open('screenshot.jpg', 'wb') as f:
+        f.write(image_data)
+
+    img = cv2.imread("screenshot.jpg")
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30)
+        )
+    if len(faces) > 0:
+        
+
+        x,y,w,h = faces[0]
+        if w > 20 :
+
+            newimg = img[y:y+h,x:x+w]
+            PIL_image = Image.fromarray(newimg)
+
+            result = [predict_gender(PIL_image),
+                      predict_glass(PIL_image),
+                      predict_Chubby(PIL_image)]
+    
+    
+            return result
+    
+    
+    
+    
 if __name__ == '__main__':
     app.run()
     app.debug = True
--- face_recognize.py
+++ face_recognize.py
@@ -110,44 +110,43 @@ def predict_Chubby(image):
 
 t1 = time.time()
 face_cascade = cv2.CascadeClassifier('/home/patrick/Desktop/morePython/cascade/cascades/haarcascade_frontalface_default.xml')
-
 # Part 2: Use fisherRecognizer on camera stream
 #face_cascade = cv2.CascadeClassifier(haar_file)
-webcam = cv2.VideoCapture(0)
-while True:
-    (_, im) = webcam.read()
-    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30))
-    if len(faces) > 0:
-        x,y,w,h = faces[0]
-        t2 = time.time()
-        if w > 20 and t2 - t1 > 1:
-            cv2.rectangle(im,(x,y),(x+w,y+h),(255,0,0),2)
-            newimg = im[y:y+h,x:x+w]
-            PIL_image = Image.fromarray(newimg)
-            p1 = predict_gender(PIL_image)
-            p2 = predict_glass(PIL_image)
-            p3 = predict_Chubby(PIL_image)
-            t1 = time.time()
-
-    for (x, y, w, h) in faces:
-        cv2.putText(im,p1,(x-10, y-30), cv2.FONT_HERSHEY_PLAIN,1,(255, 0, 0))
-        cv2.putText(im,p2,(x-10, y-20), cv2.FONT_HERSHEY_PLAIN,1,(255, 0, 0))
-        cv2.putText(im,p3,(x-10, y-10), cv2.FONT_HERSHEY_PLAIN,1,(255, 0, 0))
-
-        cv2.rectangle(im, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-    # Display the resulting frame
-    cv2.imshow('Video', im)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-
-# When everything is done, release the capture
-webcam.release()
-cv2.destroyAllWindows()
+#webcam = cv2.VideoCapture(0)
+#while True:
+#    (_, im) = webcam.read()
+#    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+#    faces = face_cascade.detectMultiScale(
+#        gray,
+#        scaleFactor=1.1,
+#        minNeighbors=5,
+#        minSize=(30, 30))
+#    if len(faces) > 0:
+#        x,y,w,h = faces[0]
+#        t2 = time.time()
+#        if w > 20 and t2 - t1 > 1:
+#            cv2.rectangle(im,(x,y),(x+w,y+h),(255,0,0),2)
+#            newimg = im[y:y+h,x:x+w]
+#            PIL_image = Image.fromarray(newimg)
+#            p1 = predict_gender(PIL_image)
+#            p2 = predict_glass(PIL_image)
+#            p3 = predict_Chubby(PIL_image)
+#            t1 = time.time()
+#
+#    for (x, y, w, h) in faces:
+#        cv2.putText(im,p1,(x-10, y-30), cv2.FONT_HERSHEY_PLAIN,1,(255, 0, 0))
+#        cv2.putText(im,p2,(x-10, y-20), cv2.FONT_HERSHEY_PLAIN,1,(255, 0, 0))
+#        cv2.putText(im,p3,(x-10, y-10), cv2.FONT_HERSHEY_PLAIN,1,(255, 0, 0))
+#
+#        cv2.rectangle(im, (x, y), (x+w, y+h), (0, 255, 0), 2)
+#
+#    # Display the resulting frame
+#    cv2.imshow('Video', im)
+#
+#    if cv2.waitKey(1) & 0xFF == ord('q'):
+#        break
+#
+#
+## When everything is done, release the capture
+#webcam.release()
+#cv2.destroyAllWindows()
