73901311bb853966ff09a4cbdec5f49e1bb2e185
ahmedelmahy <elmahy2005@gmail.com>
2019-04-22 23:36:29 +0200

    added support for apt

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ def index(name=None):
 def process_signal():
     pixels = request.get_json()['data']
     result=im2info(pixels)
+    
     if result:
         return jsonify(gender=result[0],
                        glass=result[1],
@@ -27,38 +28,42 @@ def process_signal():
         return jsonify(result = "0");
 
 def im2info(pixels):
-    dataUrlPattern = re.compile('data:image/(png|jpeg);base64,(.*)$')
-    image_data = dataUrlPattern.match(pixels).group(2)
-    image_data = image_data.encode()
-    image_data = base64.b64decode(image_data)
- 
-    with open('screenshot.jpg', 'wb') as f:
-        f.write(image_data)
-
-    img = cv2.imread("screenshot.jpg")
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    faces = face_cascade.detectMultiScale(
-            gray,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30, 30)
-        )
-    if len(faces) > 0:
+    try:
+        dataUrlPattern = re.compile('data:image/(png|jpeg);base64,(.*)$')
+        image_data = dataUrlPattern.match(pixels).group(2)
+        image_data = image_data.encode()
+        image_data = base64.b64decode(image_data)
+            
+        with open('screenshot.jpg', 'wb') as f:
+            f.write(image_data)
+    
+        img = cv2.imread("screenshot.jpg")
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+        faces = face_cascade.detectMultiScale(
+                gray,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(30, 30)
+            )
+        if len(faces) > 0:
+            
+    
+            x,y,w,h = faces[0]
+            if w > 20 :
+    
+                newimg = img[y:y+h,x:x+w]
+                PIL_image = Image.fromarray(newimg)
+    
+                result = [predict_gender(PIL_image),
+                          predict_glass(PIL_image),
+                          predict_Chubby(PIL_image)]
         
-
-        x,y,w,h = faces[0]
-        if w > 20 :
-
-            newimg = img[y:y+h,x:x+w]
-            PIL_image = Image.fromarray(newimg)
-
-            result = [predict_gender(PIL_image),
-                      predict_glass(PIL_image),
-                      predict_Chubby(PIL_image)]
-    
-    
-            return result
+        
+                return result
+    except:
+        result = [".",".","."]
+        return result
     
     
     
