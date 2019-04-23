16e7e6dbac3634ca48dba3fac743386203622621
ahmedelmahy <elmahy2005@gmail.com>
2019-04-23 17:50:09 +0200

    added camera after request

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, jsonify, request
-import cv2
 from PIL import Image
 import re
-from face_recognize import face_cascade, predict_gender, predict_glass, predict_Chubby
+from face_recognize import predict_gender, predict_glass, predict_Chubby
 import base64
-
-
+import io
+import numpy
+from mtcnn.mtcnn import MTCNN
 app = Flask(__name__)
 
 @app.route('/')
@@ -17,7 +17,8 @@ def index(name=None):
 @app.route('/snap_a_signal', methods=["POST", "GET"])
 def process_signal():
     pixels = request.get_json()['data']
-    result=im2info(pixels)
+    detector = MTCNN()
+    result=im2info(pixels, detector)
     
     if result:
         return jsonify(gender=result[0],
@@ -27,43 +28,64 @@ def process_signal():
     else:
         return jsonify(result = "0");
 
-def im2info(pixels):
-    try:
-        dataUrlPattern = re.compile('data:image/(png|jpeg);base64,(.*)$')
-        image_data = dataUrlPattern.match(pixels).group(2)
-        image_data = image_data.encode()
-        image_data = base64.b64decode(image_data)
-            
-        with open('screenshot.jpg', 'wb') as f:
-            f.write(image_data)
-    
-        img = cv2.imread("screenshot.jpg")
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
-        faces = face_cascade.detectMultiScale(
-                gray,
-                scaleFactor=1.1,
-                minNeighbors=5,
-                minSize=(30, 30)
-            )
-        if len(faces) > 0:
-            
-    
-            x,y,w,h = faces[0]
-            if w > 20 :
-    
-                newimg = img[y:y+h,x:x+w]
-                PIL_image = Image.fromarray(newimg)
-    
-                result = [predict_gender(PIL_image),
-                          predict_glass(PIL_image),
-                          predict_Chubby(PIL_image)]
+def im2info(pixels, detector):
+    #try:
+    dataUrlPattern = re.compile('data:image/(png|jpeg);base64,(.*)$')
+    image_data = dataUrlPattern.match(pixels).group(2)
+    image_data = image_data.encode()
+    image_data = base64.b64decode(image_data)
+#        
+#    with open('screenshot.jpg', 'wb') as f:
+#        f.write(image_data)
+##        
+#        
         
-        
-                return result
-    except:
-        result = [".",".","."]
-        return result
+    #fh = open("imageToSave.png", "wb")
+    #fh.write(str(pixels.split(",")[1].decode('base64')))
+    #fh.close()
+    #img = cv2.imread("screenshot.jpg")
+    #image = Image.fromstring('F', (300,300), pixels, 'raw', 'F;16')
+    #image.convert('L').save('out.png')
+   
+#        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#    
+#        faces = face_cascade.detectMultiScale(
+#                gray,
+#                scaleFactor=1.1,
+#                minNeighbors=5,
+#                minSize=(15, 15)
+#            )
+    image = Image.open(io.BytesIO(image_data))
+    #image = Image.open('imageToSave.png')
+    
+    #png = Image.open(object.logo.path)
+    image.load() # required for png.split()
+
+    background = Image.new("RGB", image.size, (255, 255, 255))
+    background.paste(image, mask=image.split()[3]) # 3 is the alpha channel
+    a = numpy.array(background)# a is readonly
+    
+    faces = detector.detect_faces(a)
+
+    if len(faces) > 0:
+        x,y,w,h = faces[0]['box']
+
+        #x,y,w,h = faces[0]
+        if w > 10 :
+
+            newimg = a[y:y+h,x:x+w]
+            print(newimg.shape)
+            PIL_image = Image.fromarray(newimg)
+
+            result = [predict_gender(PIL_image),
+                      predict_glass(PIL_image),
+                      predict_Chubby(PIL_image)]
+    
+    
+            return result
+    #except:
+        #result = [".",".","."]
+       # return result
     
     
     
